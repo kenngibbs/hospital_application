500fe3ea9f55d63203e65d9537ee3c5b0e73bcb8
ken <ken@users.noreply.github.com>
2020-01-07 00:13:35 -0600

    Changed the CSS of all pages. Decided to change the index page again. Need to update the new contact and home page to match it.

--- stratHealthProject/stratHealthApp/forms.py
+++ stratHealthProject/stratHealthApp/forms.py
@@ -1,8 +1,22 @@
-from django.forms import ModelForm
+# from django.forms import ModelForm
 from .models import Contact
+from django import forms
 
 
-class ContactForm(ModelForm):
+class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         exclude = ["contact_django_user"]
+        labels = {
+            'contact_name': "Name",
+            'contact_address': "Address",
+            'contact_phone': "Phone number",
+            'contact_position': "Job Title",
+            'contact_hospital_list': "Select Hospitals You Work With:"
+        }
+        widgets = {
+            'contact_name': forms.TextInput(attrs={'class': 'new_contact_form'}),
+            'contact_address': forms.TextInput(attrs={'class': 'new_contact_form'}),
+            'contact_phone': forms.TextInput(attrs={'class': 'new_contact_form'}),
+            'contact_position': forms.TextInput(attrs={'class': 'new_contact_form'}),
+        }
